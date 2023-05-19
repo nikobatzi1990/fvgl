fa4f8b1d0b2f6536c6aa1bed1021e090b66b7775
nikobatzi1990 <nicole.boci@gmail.com>
2023-05-19 12:15:58 +0900

    feat: write Game table code

--- videogames/models.py
+++ videogames/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Game (models.Model):
+    title = models.CharField(max_length=50)
+    release_year = models.IntegerField()
+    developer = models.CharField(max_length=50)
+    genre = models.CharField(max_length=50)
+    

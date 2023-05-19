99342fb225de4eecbd5522e0b8ab02fb1dc23fee
nikobatzi1990 <nicole.boci@gmail.com>
2023-05-19 12:23:54 +0900

    feat: write first migration

--- djangoPostgres/settings.py
+++ djangoPostgres/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.env('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'videogames.apps.VideogamesConfig'
 ]
 
 MIDDLEWARE = [

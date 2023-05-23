70def195152b66c49e64277132e36b6f302fe409
nikobatzi1990 <nicole.boci@gmail.com>
2023-05-23 11:02:53 +0900

    feat: create Input component

---
+++ client/src/Input.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Input = (props) => {
+  const { className, label, labelName, placeholder, onChange } = props;
+  
+  return(
+    <div>
+      <label htmlFor={ label }>{ labelName } </label>
+      <input
+        className = { className }
+        value = { value }
+        placeholder = { placeholder }
+        onChange={ onChange }>
+      </input>
+    </div>
+  )
+}
+
+export default Input;

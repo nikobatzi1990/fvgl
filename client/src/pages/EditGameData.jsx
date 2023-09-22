1d3e43b3dddd06475c048fca4bc04bd1b72089f3
nikobatzi1990 <nicole.boci@gmail.com>
2023-09-22 19:23:45 +0900

    feat: add form for editing game data

--- client/src/pages/EditGameData.jsx
+++ client/src/pages/EditGameData.jsx
@@ -7,30 +7,102 @@ import Input from '../components/Input';
 import Button from '../components/Button';
 
 function EditGameData () {
-
   const gameId = useParams();
   const [game, setGame] = useState({});
-
-  useEffect(() => {
-    console.log(gameId);
-  }, []);
+  const [title, setTitle] = useState('');
+  const [year, setYear] = useState('');
+  const [developer, setDeveloper] = useState('');
+  const [genre, setGenre] = useState('');
 
   const handleGameData = useCallback(async () => {
     const gameData = await axios.get(`/api/games/game/${gameId.game}/`)
     .then((result) => result.data)
     .catch((err) => console.log("ERROR: ", err))
-    console.log('🥰', gameData);
     setGame(gameData);
   }, []);
 
   useEffect(() => {
     handleGameData();
-    console.log('😶‍🌫️', game)
-  }, []);
+  }, [handleGameData]);
+
+  useEffect(() => {
+    setTitle(game.title);
+    setYear(game.release_year);
+    setDeveloper(game.developer);
+    setGenre(game.genre);
+  }, [game]);
+
+  const handleTitleInput = (e) => {
+    setTitle(e.target.value);
+  }
+
+  const handleYearInput = (e) => {
+    setYear(e.target.value);
+  }
+
+  const handleDeveloperInput = (e) => {
+    setDeveloper(e.target.value);
+  }
+
+  const handleGenreInput = (e) => {
+    setGenre(e.target.value);
+  }
+
+  const handleSubmit = async () => {
+    const editedGame = {
+      title: title,
+      release_year: year,
+      developer: developer,
+      genre: genre
+    }
+
+    await axios.patch(`/api/games/${gameId.game}/edit/`, editedGame);
+  }
+
   return (
     <>
       <Header
           text="Edit Game Data" />
+
+<div className="form">
+        <form
+          onSubmit = { handleSubmit } >
+
+          <label>
+            { "Game Title:" }
+            <Input
+              placeholder={ title }
+              onChange={ handleTitleInput } ></Input>
+          </label>
+
+          <label>
+            { "Release Year:" }
+            <Input
+              placeholder={ year }
+              onChange={ handleYearInput } ></Input>
+          </label>
+
+          <label>
+            { "Developer:" }
+            <Input
+              placeholder={ developer }
+              onChange={ handleDeveloperInput } ></Input>
+          </label>
+
+          <label>
+            { "Genre:" }
+            <Input
+              placeholder={ genre }
+              onChange={ handleGenreInput } ></Input>
+          </label>
+
+          <Button 
+            className="submission__button"
+            type="submit"
+            text="Submit"/>
+
+        </form>
+      </div>
 
       <Footer />
     </>

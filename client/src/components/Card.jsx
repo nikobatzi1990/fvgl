253abc727c63be7a679f031f7bbb70f963069505
nikobatzi1990 <nicole.boci@gmail.com>
2023-10-05 23:46:34 +0900

    style: re-organize Card layout

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -4,15 +4,18 @@ import '../styles/Card.css';
 const Card = (props) => {
   const { className, title, developer, year, genre } = props;
   return (
-    <div className="card">
+    <div className={ className } >
       <h2>{ title }</h2>
-      <div className={ className } >
-        <p>{ developer }</p>
-        <p>{ year }</p>
-        <p>{ genre }</p>
+      <div className='game__data'>
+        <div>
+          <p>{ developer }</p>
+          <p>{ year }</p>
+          <p>{ genre }</p>
+        </div>
+        <div className="image__div">Image</div>
       </div>
-        <div className="image__div">Image Placeholder</div>
     </div>
+ 
   )
 }
 

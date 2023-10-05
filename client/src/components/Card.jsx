7327e9a337ab5cb008b931922fd49216f38f0773
nikobatzi1990 <nicole.boci@gmail.com>
2023-10-05 23:32:11 +0900

    feat: add image placeholder

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -4,12 +4,14 @@ import '../styles/Card.css';
 const Card = (props) => {
   const { className, title, developer, year, genre } = props;
   return (
-    <div 
-      className={ className } >
-      <h2>{ title }</h2>
-      <p>{ developer }</p>
-      <p>{ year }</p>
-      <p>{ genre }</p>
+    <div className="card">
+      <div className={ className } >
+        <h2>{ title }</h2>
+        <p>{ developer }</p>
+        <p>{ year }</p>
+        <p>{ genre }</p>
+      </div>
+        <div className="image__div">Image Placeholder</div>
     </div>
   )
 }

12bb36d75c3ba665521059e877ec8fa49c3d03c7
nikobatzi1990 <nicole.boci@gmail.com>
2025-06-10 00:05:25 -0400

    feat: handle request to backend

--- client/src/pages/Submission.jsx
+++ client/src/pages/Submission.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import Header from "../components/Header";
@@ -14,10 +14,6 @@ function Submission() {
   const [developer, setDeveloper] = useState("");
   const [genre, setGenre] = useState("");
   const [image, setImage] = useState({});
-
-  useEffect(() => {
-    console.log("❤️", image);
-  }, [image]);
 
   const handleTitleInput = (e) => {
     setTitle(e.target.value);
--- client/src/pages/ViewGame.jsx
+++ client/src/pages/ViewGame.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-// import axios from "axios";
+import axios from "axios";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Button from "../components/Button";
@@ -8,14 +8,25 @@ import Button from "../components/Button";
 function ViewGame() {
   const navigate = useNavigate();
   const gameId = useParams();
+  const [game, setGame] = useState({});
+
+  const fetchGame = useCallback(async () => {
+    try {
+      const { data } = await axios.get(`/api/games/game/${gameId.game}`);
+      setGame(data);
+    } catch (err) {
+      console.log("ERROR: ", err);
+    }
+  }, []);
 
   useEffect(() => {
-    console.log(gameId);
-  }, []);
+    fetchGame();
+    console.log("🕹️", game);
+  }, [fetchGame]);
 
   return (
     <div>
-      <Header text="" />
+      <Header text={game.title} />
       <Button
         className="btn btn-regular align-self-start"
         onClick={() => navigate("/")}

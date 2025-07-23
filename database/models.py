97ea2210a8b5961af87faf55156df4f8a73a50f8
nikobatzi1990 <nicole.boci@gmail.com>
2025-07-22 23:54:39 -0400

    chore: make migration for list type genre

--- database/models.py
+++ database/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
 class Game (models.Model):
   title = models.CharField(max_length=50)
   release_year = models.IntegerField()
   developer = models.CharField(max_length=50)
-  genre = models.CharField(max_length=50)
+  genre = ArrayField(models.CharField(max_length=200, blank=True, null=True))
+  genre_old = models.CharField(max_length=50, blank=True, null=True, default=list)
   comment = models.TextField(blank=True, null=True)
   image_url = models.ImageField(upload_to='images/', blank=True, null=True)

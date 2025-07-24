7d0bfbc17328d7cca57953dd10102cc898ab1669
nikobatzi1990 <nicole.boci@gmail.com>
2025-07-24 00:15:29 -0400

    chore: delete old genre value

--- database/models.py
+++ database/models.py
@@ -6,6 +6,5 @@ class Game (models.Model):
   release_year = models.IntegerField()
   developer = models.CharField(max_length=50)
   genre = ArrayField(models.CharField(max_length=200, blank=True, null=True))
-  genre_old = models.CharField(max_length=50, blank=True, null=True, default=list)
   comment = models.TextField(blank=True, null=True)
   image_url = models.ImageField(upload_to='images/', blank=True, null=True)
